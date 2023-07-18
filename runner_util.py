54e947c89ecb3baae5ee14389a8ea5d8d368c202
fcormier <felixcormier@gmail.com>
2023-07-18 09:18:58 -0700

    Adding regression functions, fixed some bugs and memory leaks

--- runner_util.py
+++ runner_util.py
@@ -50,7 +50,9 @@ def make_split_file(h5_file,train_val_test_split=[0.70,0.15], output_path='data/
     dwall_cut = calc_dwall_cut(h5_file, dwall_cut_value)
     print(f'WARNING: Removing veto events')
     print(f'WARNING: Removing range=-999 events')
-    indices_to_keep = np.array(range(len(dwall_cut)))[np.logical_and(np.logical_and(dwall_cut, ~h5py.File(h5_file,mode='r')['veto'][:]), h5py.File(h5_file,mode='r')['primary_charged_range'] != -999)]
+    indices_to_keep = np.array(range(len(dwall_cut)))[np.logical_and(np.logical_and(dwall_cut, ~h5py.File(h5_file,mode='r')['veto'][:]), np.ravel(h5py.File(h5_file,mode='r')['primary_charged_range']) != -999)]
+    #indices_to_keep = np.array(range(len(dwall_cut)))[np.logical_and(dwall_cut, ~h5py.File(h5_file,mode='r')['veto'][:])]
+    print(f'indices to keep: {len(indices_to_keep)}')
     #Based on root files, divide indices into train/val/test
     length_rootfiles = len(unique_root_files)
 
